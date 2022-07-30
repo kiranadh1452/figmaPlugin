fb0e6378fab255469a8c4a7119ca18a05800e81c
Kiran Adhikari <kiranadh1452@gmail.com>
2022-07-30 19:44:54 +0545

    UI design complete

--- code.ts
+++ code.ts
@@ -1,10 +1,3 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
@@ -12,22 +5,27 @@ figma.showUI(__html__);
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  // currently only ui is designed
+  // message has not yet been passed
+
+  switch (msg.type) {
+    case "first-name":
+      // code
+    case "last-name":
+      // code
+    case "email":
+      // code
+    case "phone":
+      // code
+    case "address":
+      // code
+    case "date":
+      // code
+    default:
+      // code
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  console.log(msg.type);
+
+  // figma.closePlugin();
 };
